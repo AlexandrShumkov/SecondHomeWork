001683599ca9261e9004e65f44d5e0a4194a22cf
AlexandrShumkov <alshumkov2@yandex.ru>
2022-01-30 14:13:40 +0300

    Создание метода печати

--- Task2/Program.cs
+++ Task2/Program.cs
@@ -4,33 +4,16 @@ for (int i = 0; i < colors.Length; i++)
 Console.ReadKey();
 Console.WriteLine();
 
-
-
-
 void PrintArray(string[] array)
 {
     for (int i = 0; i < array.Length; i++)
         Console.Write(array[i] + " ");
 }
 
-string[] RemoveOdd(string[] colors)
-{
-    int count = 0;
-    string[] resultArr = new string[count];
-    for (int i = 0; i < colors.Length; i++)
-    {
-        if (colors[i].Length <= 3)
-        {
-            Array.Resize(ref resultArr, resultArr.Length + 1);
-            resultArr[resultArr.Length - 1] = colors[i];
-        }
 
-    }
-    return resultArr;
-}
 
-string[] colors2 = RemoveOdd(colors);
-PrintArray(colors2);
+
+
 
 
 
