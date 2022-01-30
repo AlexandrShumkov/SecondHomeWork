56a9d70c26d5003023131855e817edd4aed61100
AlexandrShumkov <alshumkov2@yandex.ru>
2022-01-30 14:15:50 +0300

    Создание метода формирования нового массива

--- Task2/Program.cs
+++ Task2/Program.cs
@@ -10,6 +10,22 @@ void PrintArray(string[] array)
         Console.Write(array[i] + " ");
 }
 
+string[] RemoveOdd(string[] colors)
+{
+    int count = 0;
+    string[] resultArr = new string[count];
+    for (int i = 0; i < colors.Length; i++)
+    {
+        if (colors[i].Length <= 3)
+        {
+            Array.Resize(ref resultArr, resultArr.Length + 1);
+            resultArr[resultArr.Length - 1] = colors[i];
+        }
+
+    }
+    return resultArr;
+}
+
 
 
 
